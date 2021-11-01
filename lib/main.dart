d5a86b6b58cc38afa8f7ec3e81b5a552815da52f
Abdelrahman Said <66129434+abdelrahmanSaid88@users.noreply.github.com>
2021-11-01 15:45:31 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyThemeData {
+  static var MainDarkGreen =const Color.fromRGBO(87, 169, 154, 1.0);
+  static var DarkGreen =const Color.fromRGBO(60, 112, 102, 1.0);
+  static var BackgroundColor =const Color.fromRGBO(239, 240, 241, 1.0);
+  static var Black = const Color.fromRGBO(0, 0, 0, 1.0);
+  static var White = const Color.fromRGBO(255, 255, 255, 1.0);
+}
+void main() {
+  runApp( MyApp());
+
+}
+class MyApp extends StatelessWidget {
+  // const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+      },
+
+    );
+  }
+}
