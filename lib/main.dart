bd3abcbaeff23d96e0ff35a0e9d59d17c4550dcb
Abdelrahman Said <66129434+abdelrahmanSaid88@users.noreply.github.com>
2021-11-12 04:31:45 +0200

    registration and refactor all data

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,12 @@ import 'package:planty/routes.dart';
 import 'package:planty/screens/Registration/sign_in.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp( const MyApp());
 
 }
 class MyApp extends StatelessWidget {
-  // const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
